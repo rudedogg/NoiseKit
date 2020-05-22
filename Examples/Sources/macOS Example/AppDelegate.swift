7fba4e969cdf6e76016503dd237d25cc12adbc3a
Austin Rude <rudedogg@users.noreply.github.com>
2020-05-22 15:09:29 -0600

    WIP

--- Examples/Sources/macOS Example/AppDelegate.swift
+++ Examples/Sources/macOS Example/AppDelegate.swift
@@ -3,29 +3,27 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+  
   var window: NSWindow!
-
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     // Create the SwiftUI view that provides the window contents.
     let contentView = ContentView()
-
+    
     // Create the window and set the content view. 
     window = NSWindow(
-        contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-        styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-        backing: .buffered, defer: false)
+      contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+      styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+      backing: .buffered, defer: false)
+    window.title = "NoiseKit macOS Example App"
     window.center()
     window.setFrameAutosaveName("Main Window")
     window.contentView = NSHostingView(rootView: contentView)
     window.makeKeyAndOrderFront(nil)
   }
-
+  
   func applicationWillTerminate(_ aNotification: Notification) {
     // Insert code here to tear down your application
   }
-
-
 }
 
